46410c83e836083a547fb892b490248cf588b1a0
Vladimir <vladimir.grekalov@gmail.com>
2022-11-24 18:11:37 +0500

    Removed MyUserPrincipal.java

--- src/main/java/com/example/animelist/controllers/UserController.java
+++ src/main/java/com/example/animelist/controllers/UserController.java
@@ -1,20 +1,14 @@
 package com.example.animelist.controllers;
 
 import com.example.animelist.models.Anime;
-import com.example.animelist.models.MyUserPrincipal;
 import com.example.animelist.models.User;
 import com.example.animelist.services.AnimeService;
 import com.example.animelist.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.nio.file.attribute.UserPrincipal;
 import java.security.Principal;
 
 @Controller
@@ -49,7 +43,7 @@ public class UserController {
     }
 
     @GetMapping("/add_anime")
-    public String addAnime(@RequestParam Long id, Principal principal) throws Exception {
+    public String addAnime(@RequestParam Long id, Principal principal) {
         Anime anime = animeService.getAnimeById(id);
         if (principal==null) return "redirect:/login";
         User user = userService.getUser(principal.getName());
